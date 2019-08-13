295b3c14b37f60f900484dc16407202562477fab
sangwoo85 <ksswy79@gmail.com>
2019-08-13 19:32:23 +0900

    Merging Test

--- src/main/java/com/project/superfarm/SuperFarmApplication.java
+++ src/main/java/com/project/superfarm/SuperFarmApplication.java
@@ -18,6 +18,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SuperFarmApplication {
 
+    /**
+     *
+     * @param : merging test
+     */
+
     public static void main(String[] args) {
         SpringApplication.run(SuperFarmApplication.class, args);
     }
