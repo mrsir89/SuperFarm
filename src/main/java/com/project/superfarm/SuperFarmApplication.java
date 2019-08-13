12cc7d1c4defb93de9637aa295ccd980db68e208
KANGIL <mrsir89#gmail.com>
2019-08-13 20:15:12 +0900

    Version2.0

--- src/main/java/com/project/superfarm/SuperFarmApplication.java
+++ src/main/java/com/project/superfarm/SuperFarmApplication.java
@@ -3,6 +3,9 @@
  * build date  2019.07.21
  *
  * log : 주말스터디 선생님 조언으로 순서잡고 시작단계
+ * //////////////////////////////////////////
+ * 
+ *  2019.08.13  Version 2.0 (GitHub setup)
  * */
 
 
@@ -11,14 +14,13 @@ package com.project.superfarm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 /*
 2019.07.21 CustomerContact 저장 추가  users/me로 리턴 받을때 contact 추가
  */
-
 @SpringBootApplication
 public class SuperFarmApplication {
-
-// kangil test
 
     public static void main(String[] args) {
         SpringApplication.run(SuperFarmApplication.class, args);
